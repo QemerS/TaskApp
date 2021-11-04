0cf2d0318a66e77eacc113277dcfcbf340b8508b
QemerS <tu201806169@code.edu.az>
2021-11-04 06:20:05 +0400

    Add project files.

---
+++ ClassLibrary1/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassLibrary1
+{
+    public class Class1
+    {
+        public int Topla(byte a, byte b)
+        {
+            return a + b;
+        }
+        public int Cix(byte a, byte b)
+        {
+            return a - b;
+        }
+    }
+}
---
+++ TaskApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using ClassLibrary1;
+
+namespace TaskApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 obyekt = new ClassLibrary1.Class1();
+            Console.WriteLine("cem "+obyekt.Topla(1, 2)+ obyekt.Cix(2, 1));
+        }
+    }
+}
+//Console app yaradın(İlk project), sonra ikinci project olaraq Class Library yaradın.
+//Class Library-də hər hansı bir class yaradıb daxilində bir neçə method yaradın.
+//Class library-nin referansını Console App-ə əlavə edin və Class Librarydə yaratdığınız methodları Cosole app-də istifadə edin.
